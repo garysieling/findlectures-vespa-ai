b6679cc3bf525daeb26279498db5a50e9b33c562
gary <gary@gary-pc.home>
2018-11-04 15:11:11 -0500

    Fixes #1 Add subtitle data

--- create_data.sh
+++ create_data.sh
@@ -9,9 +9,12 @@ ROOT=/projects/data/videos
 for d in $ROOT/*; do
   for j in $d/*.info.json; do
     API_FILE=$d/api.json
+    SUBTITLES_FILE=$d/subtitles.json
 
     if [ -f $API_FILE ]; then
-      jq -s '.[0] * .[1]' $j $d/api.json | jq -c '{put: ("id:video:video::" + .id), fields: {id: .id, tags: .tags, title: .title, description: .description, like_count: .like_count, dislike_count: .dislike_count, thumbnail: .thumbnail, view_count: .view_count, upload_date: .upload_date, duration: .duration, embeddable:(if .items[0].status.embeddable then 1 else 0 end),relevantTopicIds:.items[0].relevantTopicIds,topicCategories:.items[0].topicCategories,embedHtml:.items[0].player.embedHtml}}' | jq -c "del(.fields[] | nulls)" >> app/videos.json
+      if [ -f $SUBTITLES_FILE ]; then
+        jq -s '.[0] * .[1] * .[2]' $j $d/api.json $d/subtitles.json | jq -c '{put: ("id:video:video::" + .id), fields: {id: .id, tags: .tags, title: .title, description: .description, like_count: .like_count, dislike_count: .dislike_count, thumbnail: .thumbnail, view_count: .view_count, upload_date: .upload_date, duration: .duration, embeddable:(if .items[0].status.embeddable then 1 else 0 end),relevantTopicIds:.items[0].relevantTopicIds,topicCategories:.items[0].topicCategories,embedHtml:.items[0].player.embedHtml, subtitles:.subtitles}}' | jq -c "del(.fields[] | nulls)" >> app/videos.json
+      fi
     fi
   done
 done
