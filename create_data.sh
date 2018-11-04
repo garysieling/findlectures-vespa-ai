7a24d36b03b8b6f7a6d60efb8947c26e55b5ec14
gary <gary@gary-pc.home>
2018-11-04 09:36:51 -0500

    Data script reads youtube API dat

--- create_data.sh
+++ create_data.sh
@@ -8,6 +8,6 @@ fi
 ROOT=/projects/data/videos
 for d in $ROOT/*; do
   for j in $d/*.info.json; do
-    cat $j | jq -c '{put: .id, fields: {id: .id, tags: .tags, title: .title, description: .description, like_count: .like_count, dislike_count: .dislike_count, thumbnail: .thumbnail, view_count: .view_count, upload_date: .upload_date, duration: .duration}}' >> app/videos.json
+    jq -s '.[0] * .[1]' $j $d/api.json | jq -c '{put: .id, fields: {id: .id, tags: .tags, title: .title, description: .description, like_count: .like_count, dislike_count: .dislike_count, thumbnail: .thumbnail, view_count: .view_count, upload_date: .upload_date, duration: .duration, embeddable:.items[0].status.embeddable,relevantTopicIds:.items[0].relevantTopicIds,topicCategories:.items[0].topicCategories,embedHtml:.items[0].player.embedHtml}}' >> app/videos.json
   done
 done
