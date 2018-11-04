ed11d9ccc9342dd96cb48381f193637f3cad14ae
gary <gary@gary-pc.home>
2018-11-04 14:48:14 -0500

    Fixes #2 Correct data types

--- create_data.sh
+++ create_data.sh
@@ -8,6 +8,10 @@ fi
 ROOT=/projects/data/videos
 for d in $ROOT/*; do
   for j in $d/*.info.json; do
-    jq -s '.[0] * .[1]' $j $d/api.json | jq -c '{put: .id, fields: {id: .id, tags: .tags, title: .title, description: .description, like_count: .like_count, dislike_count: .dislike_count, thumbnail: .thumbnail, view_count: .view_count, upload_date: .upload_date, duration: .duration, embeddable:.items[0].status.embeddable,relevantTopicIds:.items[0].relevantTopicIds,topicCategories:.items[0].topicCategories,embedHtml:.items[0].player.embedHtml}}' >> app/videos.json
+    API_FILE=$d/api.json
+
+    if [ -f $API_FILE ]; then
+      jq -s '.[0] * .[1]' $j $d/api.json | jq -c '{put: ("id:video:video::" + .id), fields: {id: .id, tags: .tags, title: .title, description: .description, like_count: .like_count, dislike_count: .dislike_count, thumbnail: .thumbnail, view_count: .view_count, upload_date: .upload_date, duration: .duration, embeddable:(if .items[0].status.embeddable then 1 else 0 end),relevantTopicIds:.items[0].relevantTopicIds,topicCategories:.items[0].topicCategories,embedHtml:.items[0].player.embedHtml}}' | jq -c "del(.fields[] | nulls)" >> app/videos.json
+    fi
   done
 done
